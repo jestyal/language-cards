c81b3e6480655108ceacd0467f7d8e8ca23516ba
jestyal <natali.doroshenko14gmail.com>
2021-12-17 18:59:17 +0500

    Fixed some styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
         <Header />
         <div className="wrapper">
-            <CardList />
+            {/*<CardList />*/}
             <WordList />
         </div>
         <Footer />
