ab0ee65a1945cab36a9bb5a5ecdea1e8d8e0f81a
jestyal <natali.doroshenko14gmail.com>
2021-12-17 16:05:19 +0500

    Add SearchForm component.

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,15 @@
+import "../../assets/styles/forms.scss";
 import "./SearchForm.scss";
-
+import { ReactComponent as SearchIcon } from './search-icon.svg';
 
 export default function SearchForm() {
     return (
-        <div className="SearchForm">
-
-        </div>
+        <form className="Form SearchForm">
+            <input id="search-input" type="text" className="form__input" placeholder="Search" />
+            <div className="SearchForm__btn-wrap">
+                <input type="submit" className="form__btn btn" value="" />
+                <SearchIcon />
+            </div>
+        </form>
     );
 }
