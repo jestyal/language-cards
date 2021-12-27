b433695b5bf913e5ce6304e499da2f99a81929f0
jestyal <natali.doroshenko14gmail.com>
2021-12-27 14:15:23 +0500

    Fixed WordList styles

--- src/components/WordList/WordList.jsx
+++ src/components/WordList/WordList.jsx
@@ -1,13 +1,20 @@
-import { Fragment } from "react";
+import {Fragment} from "react";
 import Word from "./Word";
 import "./Word.scss";
 import WORDS from "../Dictionary/Dictionary";
+import EditButton from "./EditButton/EditButton";
 
 function WordList() {
     return (
         <Fragment>
             <h2 className="title">Word list</h2>
             <div className="Word__list">
+                <div className="Word__list-head">
+                    <div>English</div>
+                    <div>Transcription</div>
+                    <div>Russian</div>
+                    <div>Edit Buttons</div>
+                </div>
                 {
                     WORDS.map((item) =>
                         <Word key={item.id}
