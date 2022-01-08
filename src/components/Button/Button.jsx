2e76d3a2b1fdca1ad01fa4a49ddb8031f4e8dd62
jestyal <natali.doroshenko14gmail.com>
2022-01-08 14:17:44 +0500

    Add styles to card slider

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,13 @@
 import "./Button.scss";
+// import { ReactComponent as ArrowPrev } from "../../assets/images/icons/arrowPrev.svg"
+// import { ReactComponent as ArrowNext } from "../../assets/images/icons/arrowNext.svg"
 
-function Button({ title, onClick, isHidden }) {
+function Button({ title, onClick, isHidden, arrowPrev, arrowNext }) {
     return (
-        <button className={`Btn ` + (isHidden ? 'js_hidden' : null)} onClick={onClick}>
+        <button className={`Btn ` + (isHidden ? 'js_hidden' : "")} onClick={onClick}>
             {title}
+            {/*{arrowPrev ? <ArrowPrev /> : null}*/}
+            {/*{arrowNext ? <ArrowNext /> : null}*/}
         </button>
     );
 }
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,17 +2,28 @@ import { Fragment } from "react";
 import Card from "./Card/Card";
 import "./Card.scss";
 import WORDS from "../Dictionary/Dictionary";
+import Button from "../Button/Button";
 
 function CardList() {
     return (
         <Fragment>
             <h2 className="title">Card list</h2>
-            <div className="card__list">
-                {
-                    WORDS.map((item) =>
-                        <Card key={item.id} {...item} />
-                    )
-                }
+
+            <div className="card__slider">
+                <div className="card__list">
+                    {
+                        WORDS.map((item) =>
+                            <Card key={item.id} {...item} />
+                        )
+                    }
+                </div>
+                <div className="card__arrows">
+                    <Button title="Prev" arrow="arrowPrev" />
+                    <Button title="Next" arrow="arrowNext" />
+                </div>
+                <div className="card__counter">
+                    1 / 10
+                </div>
             </div>
         </Fragment>
     );
