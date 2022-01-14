8d26ebbc73b4bca55023f0a889a55a3224451c22
jestyal <natali.doroshenko14gmail.com>
2022-01-14 17:21:21 +0500

    Changed 404 page

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -3,11 +3,16 @@ import {Link} from "react-router-dom";
 
 function PageNotFound() {
     return (
-        <section className="wrapper wrapper_full text_center">
-            <h1 className="title title_max mt_40 mb_40">404</h1>
-            <h2 className="title title_max">Page not found</h2>
-            <p>Return to <Link to="/">home</Link></p>
-        </section>
+        <main className="wrapper wrapper_error-page">
+            <div className="">
+                <h1 className="title title_error-page">404</h1>
+                <h2 className="title title_max">Page not found</h2>
+                <div className="mb_40">
+                    <img src="https://i.gifer.com/3z9a.gif" alt="404"/>
+                </div>
+                <p className="title title_max">Please, return to <Link to="/">home page</Link></p>
+            </div>
+        </main>
     );
 }
 
