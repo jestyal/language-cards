b72dd845948c22e0652fa92cf879c93a472c6987
jestyal <natali.doroshenko14gmail.com>
2022-02-09 12:17:29 +0500

    Added Loading Indicator

---
+++ src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import "./LoadingIndicator.scss";
+
+export default function LoadingIndicator() {
+    return (
+        <div className="loading-indicator__wrap">
+            <div className="loading-indicator"></div>
+        </div>
+    )
+}
