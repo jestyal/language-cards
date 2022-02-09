1e40084ac8fd580bd61be782c51ed4419b6fee10
jestyal <natali.doroshenko14gmail.com>
2022-02-09 12:25:18 +0500

    Fixed styles Loading Indicator

--- src/components/LoadingIndicator/LoadingIndicator.jsx
+++ src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -4,6 +4,7 @@ export default function LoadingIndicator() {
     return (
         <div className="loading-indicator__wrap">
             <div className="loading-indicator"></div>
+            <div className="title mt_40">Loading...</div>
         </div>
     )
 }
